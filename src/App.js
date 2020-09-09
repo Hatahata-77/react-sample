e4945ea54c61dfda05631646657bf1c34c963846
Takahiro Hata <taka-h@ryobi.co.jp>
2020-09-10 07:14:27 +0900

    redux

--- src/App.js
+++ src/App.js
@@ -1,56 +1,70 @@
-import React, { Component } from 'react';
-import {BrowserRouter, Route, Link} from 'react-router-dom'
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+import { createStore } from 'redux'
 
-function App() {
+const App = () => (
+  <BrowserRouter>
+    <div class="container text-center mt-5">
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/blog/:id" component={Blog} />
+      <Route path="/sum/:num1/:num2" component={Sum} />
+    </div>
+  </BrowserRouter>
+)
+
+const vote = (state=0, action) => {
+  switch (action.type) {
+    case 'ADD':
+      return state + 1
+    case 'MINUS':
+      return state - 1
+    default:
+      return state
+  }
+}
+
+let store = createStore(vote)
+
+const Home = () => {
+  store.dispatch({type: 'ADD'})
+  let x = store.getState().toString()
   return (
-    <div className="container text-center">
-      <Clock />
+    <div>
+      <h1>Welcom</h1>
+      <p>投票数： {x}</p>
+      <p>
+        <Link to="/about">Aboutへ</Link>
+      </p>
     </div>
   )
-  // const items = [
-  //   {"name": "イチゴ", "price": "180"},
-  //   {"name": "りんご", "price": "150"},
-  //   {"name": "バナナ", "price": "230"}
-  // ]
-
-  // return (
-  //   <div className="App">
-  //     <table className="table table-striped">
-  //       <tbody>
-  //         {items.map(value => (
-  //           <tr>
-  //             <th scope="row">{value.name}</th>
-  //             <td>{value.price}</td>
-  //           </tr>
-  //         ))}
-  //       </tbody>
-  //     </table>
-  //   </div>
-  // )  
 }
 
-class Clock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.now = new Date();
-    this.state = {
-      time: `${this.now.getHours()}:${this.now.getMinutes()}:${this.now.getSeconds()}`    
-    }
-
-    this.refresh = this.refresh.bind(this);
-  }
-
-  refresh() {
-    this.now = new Date();
-    this.setState(state => ({
-      time: `${this.now.getHours()}:${this.now.getMinutes()}:${this.now.getSeconds()}`    
-    }));
-  }
-  render() {
-    return <p onClick={this.refresh}>{this.state.time}</p>
-  }
+const About = () => {
+  store.dispatch({type: 'MINUS'})
+  return (
+    <div>
+      <h1>About</h1>
+    </div>
+  )
 }
 
-export default App;
+const Blog = props => {
+  const { id } = props.match.params
+  return (
+    <div>
+      <p>{id}番目の記事です</p>
+    </div>
+  )
+}
+
+const Sum = props => {
+  const { num1, num2 } = props.match.params
+  return (
+    <div>
+      <p>{num1} + {num2} = {parseInt(num1) + parseInt(num2)}</p>
+    </div>
+  )
+}
+
+export default App
